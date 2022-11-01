164eb1d98cada81b8dac829935a2fd67f0ad9266
Noam Schuldiner <noamschuldiner@gmail.com>
2022-11-01 20:37:41 +0200

    testing communication with ui

--- Python/uicomms.py
+++ Python/uicomms.py
@@ -1,5 +1,6 @@
 from socket import *
 import os
+from card_classes import *
 
 class UiComms:
     def __init__(self):
@@ -13,15 +14,42 @@ class UiComms:
         self.open_ui_file()
 
     def init_sock(self):
-        self.sock = socket(family=AF_INET, type=SOCK_DGRAM)
+        self.sock = socket(family=AF_INET, type=SOCK_STREAM)
         self.sock.bind(("127.0.0.1", self.localPort))
+
+        self.sock.listen(3)
+        conn, addr = self.sock.accept()
+        print(f"Connected to ui at {addr}")
 
     def open_ui_file(self):
         os.startfile(self.ui_file_name)
 
-    def send_msg_to_ui(self, msg):
-        self.sock.sendto(msg, self.client_addr)
+    def send_msg_to_ui(self, code, data):
+        num_to_letter = {
+            1: "S",
+            2: "C",
+            3: "D",
+            4: "H"
+        }
+
+        if code == 1:
+            message = "01-" + data + "!"
+        elif code == 2:
+            message = "02"
+            for card in data:
+                message += "-" + num_to_letter[card.suit.value] + card.rank.value
+            message += "!"
+        elif code == 3:
+            card = num_to_letter[data[1].suit.value] + data[1].rank.value
+            message = "03-" + data[0] + "-" + card + "!"
+        elif code == 4:
+            message = "04-" + str(data) + "!"
+        else:
+            return
+        self.sock.sendto(bytes(message), self.client_addr)
 
 
 if __name__ == '__main__':
     ui = UiComms()
+    c = Card(Suit[1], Rank[8])
+    ui.send_msg_to_ui(3, (2, c))
