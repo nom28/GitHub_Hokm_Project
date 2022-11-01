c3ef02ef1ea4dad1c641f9999e3893be022c5a91
Amihai Ratson <amihai2005@gmail.com>
2022-11-01 20:59:06 +0200

    fixes

--- Python/uicomms.py
+++ Python/uicomms.py
@@ -4,21 +4,21 @@ from card_classes import *
 
 class UiComms:
     def __init__(self):
-        self.localPort = 55556
+        self.localPort = 55557
         self.BUFF_SIZE = 1024
         self.client_addr = ("127.0.0.1", 55557)
 
         self.ui_file_name = os.getcwd() + "\\HokmAI\\HokmAI\\bin\\Debug\\HokmAI.exe"
 
         self.init_sock()
-        self.open_ui_file()
+        #self.open_ui_file()
 
     def init_sock(self):
         self.sock = socket(family=AF_INET, type=SOCK_STREAM)
         self.sock.bind(("127.0.0.1", self.localPort))
 
         self.sock.listen(3)
-        conn, addr = self.sock.accept()
+        self.conn, addr = self.sock.accept()
         print(f"Connected to ui at {addr}")
 
     def open_ui_file(self):
@@ -40,16 +40,16 @@ class UiComms:
                 message += "-" + num_to_letter[card.suit.value] + card.rank.value
             message += "!"
         elif code == 3:
-            card = num_to_letter[data[1].suit.value] + data[1].rank.value
-            message = "03-" + data[0] + "-" + card + "!"
+            card = num_to_letter[data[1].suit.value] + str(data[1].rank.value)
+            message = "03-" + str(data[0]) + "-" + card + "!"
         elif code == 4:
             message = "04-" + str(data) + "!"
         else:
             return
-        self.sock.sendto(bytes(message), self.client_addr)
+        self.conn.send(message.encode("utf-8"))
 
 
 if __name__ == '__main__':
     ui = UiComms()
-    c = Card(Suit[1], Rank[8])
+    c = Card(Suit["DIAMONDS"], Rank["rank_J"])
     ui.send_msg_to_ui(3, (2, c))
