8e6eed2207df9e8c886a7cfe8dba87042ae0224f
Amihai Ratson <amihai2005@gmail.com>
2022-11-04 17:32:29 +0200

    fixes bugs.

--- Python/uicomms.py
+++ Python/uicomms.py
@@ -11,8 +11,8 @@ class UiComms:
 
         self.ui_file_name = os.getcwd() + "\\HokmAI\\HokmAI\\bin\\Debug\\HokmAI.exe"
 
-        # self.init_sock()
-        # self.open_ui_file()
+        self.open_ui_file()
+        self.init_sock()
 
     def init_sock(self):
         self.sock = socket(family=AF_INET, type=SOCK_STREAM)
@@ -47,7 +47,8 @@ class UiComms:
             message = "04-" + str(data) + "!"
         else:
             return
-        # self.conn.send(message.encode("utf-8"))
+        self.conn.send(message.encode("utf-8"))
+        print(message)
 
 
 if __name__ == '__main__':
